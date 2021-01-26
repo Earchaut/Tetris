c966e5e18368e4bcbdebff12d38bb82428066613
KSCN <hbsjzliuyang@163.com>
2021-01-26 10:36:59 +0800

    添加项目文件。

---
+++ content.h
@@ -0,0 +1,11 @@
+#pragma once
+class form {
+	int content[200];
+	int create_i();
+	int create_o();
+	int create_t();
+	int create_l();
+	int create_j();
+	int create_z();
+	int create_s();
+};
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include<Windows.h>
+using namespace std;
+
+int main() {
+
+}
